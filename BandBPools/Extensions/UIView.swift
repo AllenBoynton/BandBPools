eb3aeebc9047abceb80f99e627675e7bb0d544c6
Allen Boynton <alboynton4@gmail.com>
2018-09-14 00:11:16 -0400

    Added constraints extension to UIView
    Down the road if I want to not use storyboards

---
+++ BandBPools/Extensions/UIView.swift
@@ -0,0 +1,24 @@
+//
+//  UIView.swift
+//  BandBPools
+//
+//  Created by Allen Boynton on 8/13/18.
+//  Copyright © 2018 Allen Boynton. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func addConstraintsWithFormat(format: String, views: UIView...) {
+        var viewsDictionary = [String: UIView]()
+        
+        for (index, view) in views.enumerated() {
+            let key = "v\(index)"
+            viewsDictionary[key] = view
+            view.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutFormatOptions(), metrics: nil, views: viewsDictionary))
+    }
+}
