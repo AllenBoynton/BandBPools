4edc148c89274cd77290e1cb8bcdb912262caabc
Allen Boynton <alboynton4@gmail.com>
2018-09-14 00:12:04 -0400

    Added webview and control buttons
    Created logic and functions to operate webview

--- BandBPools/MenuVC.swift
+++ BandBPools/MenuVC.swift
@@ -9,15 +9,35 @@
 import UIKit
 import FirebaseAuth
 import SwiftMessages
+import WebKit
 
 class MenuVC: UIViewController {
     
+    @IBOutlet weak var forwardButton: UIButton!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var webView: WKWebView!
     
+    @IBOutlet weak var urlTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        createMessage()
+//        createMessage()
+        urlTextField.delegate = self
+        webView.navigationDelegate = self
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        handleWebView()
+    }
+    
+    func handleWebView() {
+        let urlString = "http://bbpemp.secureasap.com/login.php"
+        let url = URL(string: urlString)!
+        let request = URLRequest(url: url)
+        webView.load(request)
+        
+        urlTextField.text = urlString
     }
     
 
@@ -27,6 +47,32 @@ class MenuVC: UIViewController {
             dismiss(animated: true, completion: nil)
         } catch {
             print("There was a problem logging out")
+        }
+    }
+    
+    @IBAction func backButtonTapped(_ sender: Any) {
+        if webView.canGoBack {
+            webView.goBack()
+        }
+    }
+    
+    @IBAction func forwardButtonTapped(_ sender: Any) {
+        if webView.canGoForward {
+            webView.goForward()
+        }
+    }
+    
+    @IBAction func stopButtonTapped(_ sender: Any) {
+        if webView.isLoading {
+            webView.stopLoading()
+        } else {
+            do {
+                try Auth.auth().signOut()
+                dismiss(animated: true, completion: nil)
+            } catch {
+                print("There was a problem logging out")
+            }
+//            self.dismiss(animated: true, completion: nil)
         }
     }
     
@@ -47,3 +93,26 @@ class MenuVC: UIViewController {
         SwiftMessages.show(view: view) // config: config,
     }
 }
+
+extension MenuVC: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        let urlString = urlTextField.text!
+        let url = URL(string: urlString)!
+        let request = URLRequest(url: url)
+        webView.load(request)
+        
+        textField.resignFirstResponder()
+        
+        return true
+    }
+}
+
+extension MenuVC: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        backButton.isEnabled = webView.canGoBack
+        forwardButton.isEnabled = webView.canGoForward
+        
+        urlTextField.text = webView.url?.absoluteString
+    }
+}
+
