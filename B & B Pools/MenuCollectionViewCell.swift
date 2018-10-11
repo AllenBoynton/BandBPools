9cf78118c0e3d3ac3e167d2b6e2aa0919147e402
Allen Boynton <alboynton4@gmail.com>
2018-10-11 16:34:51 -0400

    Created MenuCollectionViewCell

---
+++ B & B Pools/MenuCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MenuCollectionViewCell.swift
+//  B & B Pools
+//
+//  Created by Allen Boynton on 10/11/18.
+//  Copyright © 2018 Allen Boynton. All rights reserved.
+//
+
+import UIKit
+
+class MenuCollectionViewCell: UICollectionViewCell {
+    
+}
