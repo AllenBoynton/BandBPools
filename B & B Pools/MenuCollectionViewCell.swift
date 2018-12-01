3f18c7de7811fa09838911c8fbf8c3d96f6ca418
Allen Boynton <alboynton4@gmail.com>
2018-11-30 21:49:23 -0500

    Update MenuCollectionViewCell.swift

--- B & B Pools/MenuCollectionViewCell.swift
+++ B & B Pools/MenuCollectionViewCell.swift
@@ -10,4 +10,27 @@ import UIKit
 
 class MenuCollectionViewCell: UICollectionViewCell {
     
+    
+//    public let separatorLineView: UIView = {
+//        let lineView = UIView()
+//        lineView.backgroundColor = UIColor(white: 0, alpha: 0.5)
+//        lineView.isHidden = true
+//        return lineView
+//    }()
+//
+//    public override init(frame: CGRect) {
+//        super.init(frame: frame)
+//        setupViews()
+//    }
+//
+//    ///Override this method to provide your own custom views.
+//    open func setupViews() {
+//        clipsToBounds = true
+//        addSubview(separatorLineView)
+//        separatorLineView.anchor(nil, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0.5)
+//    }
+//
+//    public required init?(coder aDecoder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
 }
