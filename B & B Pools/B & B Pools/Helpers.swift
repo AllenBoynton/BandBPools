f361f5209f0076e62a67dabb24e4df6bd0610a6e
Allen Boynton <alboynton4@gmail.com>
2019-02-21 21:39:14 -0500

    Create Helpers.swift

---
+++ B & B Pools/B & B Pools/Helpers.swift
@@ -0,0 +1,13 @@
+//
+//  Helpers.swift
+//  B & B Pools
+//
+//  Created by Allen Boynton on 2/11/19.
+//  Copyright © 2019 Allen Boynton. All rights reserved.
+//
+
+import Foundation
+
+var loginVsSignup = UInt()
+
+let names = ["Pool Shapes", "Liner Patterns", "Fiberglass", "Waterfalls", "Fireplaces", "Furniture", "Loungers", "Fire Pits", "Fences", "Grills", "Service", "Financing", "Facebook", "B & B Website", "Instagram"]
